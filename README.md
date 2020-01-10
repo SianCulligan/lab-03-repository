d20b1910e03dbb4e856c3f827a33a3611050b3c3
Sian Culligan <SianCulligan@gmail.com>
2020-01-09 18:30:41 -0800

    adding base code and file structure

---
+++ README.md
@@ -0,0 +1,18 @@
+# lab-03
+
+Number and name of feature: ________________________________
+
+* Feature #1: Display images
+* Feature #2: Filter images
+* Feature #3: Style the application
+
+Estimate of time needed to complete: _3 hours_
+* Feature #1: Display images 1.5 hours
+* Feature #2: Filter images 45 minutes
+* Feature #3: Style the application 45 mins
+
+Start time: 6:17pm
+
+Finish time: _____
+
+Actual time needed to complete: _____
---
+++ js/app.js
@@ -0,0 +1,148 @@
+'use strict';
+
+// Feature 1:
+// - As a user, I want to view the images on the page so that I can browse the photo collection
+
+// 1. X open an Array
+// 2. X create a constructor function
+// 3. Build a prototype
+// 4. .Get using AJAX
+// 5. .append to the DOM
+
+let imgArr = [];
+
+function Image (imgObject) {
+  this.image_url = imgObject.image_url;
+  this.title = imgObject.title;
+  this.description = imgObject.description;
+  this.keyword = imgObject.keyword;
+  this.horns = imgObject.horns;
+}
+
+// render function
+Image.prototype.render = function() {
+  $('main').append('<div class="clone"></div>');
+  let imageClone = $('div[class="clone"]');
+  let imageHtml = $('#photo-template').html();
+  imageClone.html(imageHtml);
+  imageClone.find('h2').text(this.title);
+  imageClone.find('img').attr('src', this.image_url); imageClone.find('img').attr('alt', this.title);
+  imageClone.find('p').text(this.description);
+  imageClone.removeClass('clone');
+  imageClone.attr('class' , this.keyword);
+};
+
+Image.readJson = () => {
+  $.ajax('../data/page-1.json','json')
+    .then(imgData => {
+      imgData.forEach(imageItem => {
+        imgArr.push(new Image(imageItem));
+      });
+    })
+    .then(Image.loadImage);
+};
+
+Image.loadImage = () => {
+  imgArr.forEach(imgObject => imgObject.render());
+  renderList();
+};
+
+$(() => Image.readJson());
+
+//loop through our array to create objects
+// create a render function
+
+
+
+// populate the dropdown with our keywords
+// write a function that responds to users click
+// when clicked, all images w/ keywords are .show, everything else is .hide
+
+function renderList () {
+  let keywordFilter = [];
+  imgArr.forEach((keywordCheck) => {
+    if(!keywordFilter.includes(keywordCheck.keyword)) {
+      console.log(keywordCheck.keyword);
+      keywordFilter.push(keywordCheck.keyword);
+      $('select').append(`<option>${keywordCheck.keyword}</option>`);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+  // check if keyword already exists (.include!!!),
+  //if not .push to the array
+  //render it to the DOM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     //selecting the parent & creating an option
+//     $('select').append('<option class="clone"></option>');
+//     $('option').append('<p></p>');
+//     let $listClone = $('option[class="clone"]');
+//     //fill the option
+//     $listClone.find('option').text(this.keyword)
+//     $listClone.removeClass('clone');
+//     $listClone.attr('class', this.keyword);
+//     //find the option
+//     $listClone.find('p').text(this.keyword)
+}
+
+
+// Feature #2:
+// 1. 'Listen' for click, on(click) run the event listener function
+// 2. .show or .fadein anything that is equal to that keyword (?? .includes ??)
+// 3. Reset button?
+
+// create a global array holding unique arrays
+// use unique array to make dropdown
+// on('select change'), collect .this .hide all, .show classes that match keyword
+
+
+// Image.prototype.renderList = function() {
+//   //selecting the parent & creating an option
+//   imgArr.forEach()
+
+//   $('option').append('<p></p>');
+//   let $listClone = $('option[class="clone"]');
+//   //fill the option
+//   $listClone.find('option').text(this.keyword)
+//   $listClone.removeClass('clone');
+//   $listClone.attr('class', this.keyword);
+//   //find the option
+//   $listClone.find('p').text(this.keyword)
+// };
